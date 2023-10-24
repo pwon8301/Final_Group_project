1bd44baf89555970075202344540ee2fe1db3789
JowyWang <145730602+JowyWang@users.noreply.github.com>
2023-10-24 18:23:49 +1100

    changes

--- Script/sketch.js
+++ Script/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background(220);
+
+}
+
+function RoadGenerate(Xpos,Ypos,Size,RoadDirection,Roadlength){
+let Xpos = Xpos;
+let Ypos = Ypos;
+let Size = Size;
+let RoadWidth = 20;
+Roadlength = 600;
+RoadDirection = 1;
+// 1 for horizontal position
+// 0 for vertical
+let Road = rect(Xpos,Ypos,RoadWidth,Roadlength)
+return Road;
 }
